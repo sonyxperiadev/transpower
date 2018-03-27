09294bcf1f81fa1159661ffad1a421f6c6e05c8f
Zoran Jovanovic <zoran.jovanovic@sony.com>
2018-03-27 10:27:19 +0200

    Update outdated javadoc

--- common/src/com/sony/transmitpower/BootCompletedReceiver.java
+++ common/src/com/sony/transmitpower/BootCompletedReceiver.java
@@ -12,37 +12,40 @@ import com.sony.transmitpower.service.TransmitPowerService;
 import com.sony.transmitpower.util.Util;
 
 /**
- * It's obvious what it does, but why is it here in the first
- * place? Shouldn't this be replaced by something like JobScheduler?
- * Well, yes and no. Yes, JobScheduler may be used to schedule
+ * BootCompletedReceiver starts the service upon receiving
+ * a BOOT_COMPLETED intent (and in later versions a
+ * LOCKED_BOOT_COMPLETED intent).
+ * Why was this design chosen?
+ * For example, a JobScheduler may be used to schedule
  * jobs based on network but they need to be setup somewhere, and
  * in case of a service that should run always when any network is
  * started that means that the job would have to be scheduled on
  * boot completed - which is sort of what we do here, so the
- * question is when and how we should instatiate all the objects
- * needed and when does the whole thing need to die.
+ * question is when and how we should instantiate all the objects
+ * needed and when the whole thing needs to die.
  * Initialization:
  * In case of service, it is on its creation when basically all
  * initialization happens and for most time the service is then
  * idle and shoots small messages around on some callbacks. So,
  * heaviest task once a boot.
  * In case of JobScheduler, initialization happens any time a
- * network is available and then it continues mostly idle until
- * the job is killed. So heaviest task multiple times during the
- * life time of device and also potentially when it's needed to
- * react quickly.
+ * network (or another trigger) is available and then it continues
+ * mostly idle until the job is killed. So heaviest task multiple
+ * times during the life time of device and also potentially when
+ * it's needed to react quickly.
  * Life:
  * In case of sticky service, it never dies or rather it's a zombie.
  * If it gets killed it will resurrect immediately. This comes in
  * handy if you have to react on every call, movement, data exchange
- * etc.
+ * etc. with a downside that that it races for resources every time
+ * it's killed (most probably when resources are scarce).
  * In case of a job, it can die whenever the system decides, even
- * if it's incovenient e.g. in need of telling modem to power down.
+ * if it's inconvenient e.g. in need of telling modem to power down.
  *
  * What then with resources taken by Service? It's a compromise
  * between resource taken always with less radiation always and
  * resources taken and released most of the time with less radiation
- * some times, i.e. live with it. I may apologize later (or not).
+ * some times.
  */
 public final class BootCompletedReceiver extends BroadcastReceiver {
     private static final String TAG = BootCompletedReceiver.class.getCanonicalName();
