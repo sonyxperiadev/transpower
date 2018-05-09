78ea17bbc6edd9229e9b44d4bb196a95a0e26fd9
oshmoun <oshmoun100@googlemail.com>
2018-05-09 13:50:16 +0200

    TransPowerBase: correct path to java files
    The path TransPowerBase/src does not exist.
    Changing it to point at ../common/src like how it is done for LOCAL_RESOURCE_DIR makes the app build.

--- TransPowerBase/Android.mk
+++ TransPowerBase/Android.mk
@@ -8,7 +8,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-java-files-under, ../common/src)
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/../common/res
 
