c1694525962783162deb4069b047f2dd62b43e79
Zoran Jovanovic <zoran.jovanovic@sony.com>
2018-05-14 17:15:51 +0200

    Fix TransPowerBase build
    Source of TransPowerBase app is actually the same as
    the source of TransPowerCommon library, so in Android O
    it was enough to have an empty source folder and link
    TransPowerCommon library as a static Android library.
    
    However:
      1. the folder was missing in release
      2. this trick doesn't work in pre-O releases
    
    The solution is to refer to appropriate source folder residing
    in the common folder and also link to appropriate libraries.
    
    Issue: #4

--- TransPowerBase/Android.mk
+++ TransPowerBase/Android.mk
@@ -8,14 +8,14 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-java-files-under, ../common/src)
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/../common/res
 
 LOCAL_JAVA_LIBRARIES += telephony-common
-LOCAL_STATIC_ANDROID_LIBRARIES += \
+LOCAL_STATIC_JAVA_LIBRARIES += \
     android-support-v4 \
-    TransPowerCommon
+    libpower
 
 # proguard:
 # use on debug
