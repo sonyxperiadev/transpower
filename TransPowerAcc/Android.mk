sonyxperiadev/transpower

commit 483927d97a6b630731ba583ac06be92c3bb78111

Fix access to telephony-common lib and additional resources
Request access to needed libraries and resources, so that
proguard doesn't have problems accessing them while
optimizing apps.

Signed-off-by: Zoran Jovanovic <zoran.jovanovic@sony.com>

TransPowerAcc/Android.mk
@@ -20,7 +20,17 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     libacc \
     libpower
 
+hidden_api_major_vers := 28
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), $(hidden_api_major_vers)),)
+    LOCAL_JAVA_LIBRARIES += telephony-common
+    LOCAL_USE_AAPT2 := true
+endif
+
 LOCAL_AAPT_FLAGS := --auto-add-overlay
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), $(hidden_api_major_vers)),)
+	LOCAL_AAPT_FLAGS += \
+		--extra-packages com.sony.transmitpower.sensor
+endif
 
 # proguard:
 #LOCAL_PROGUARD_ENABLED := disabled

TransPowerProx/Android.mk
@@ -19,6 +19,10 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     libpower \
     libprox
 
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 28),)
+    LOCAL_JAVA_LIBRARIES += telephony-common
+endif
+
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 # proguard:

TransPowerSensors/Android.mk
@@ -19,7 +19,17 @@ LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 
 LOCAL_STATIC_JAVA_LIBRARIES += libacc libpower libprox
 
+hidden_api_major_vers := 28
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), $(hidden_api_major_vers)),)
+    LOCAL_JAVA_LIBRARIES += telephony-common
+    LOCAL_USE_AAPT2 := true
+endif
+
 LOCAL_AAPT_FLAGS := --auto-add-overlay
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), $(hidden_api_major_vers)),)
+	LOCAL_AAPT_FLAGS += \
+		--extra-packages com.sony.transmitpower.sensor
+endif
 
 # proguard:
 #LOCAL_PROGUARD_ENABLED := disabled

commit 87fd2ea726936b4e7417850486cf18b29637af05

TransPower*: Drop unused, forbidden static lib telephony-common
Since API 28 it is not allowed to link against internal system libraries
anymore. This library is not used anyway as all the relevant code is
already using the appropriate public APIs available in Android.

Fixes:
vendor/oss/transpower/common/Android.mk: error: TransPowerCommon
(java:system) should not link to telephony-common (java:platform)

Fixes: 483927d97a6b630731ba583ac06be92c3bb78111
Signed-off-by: Marijn Suijten <marijns95@gmail.com>

TransPowerAcc/Android.mk
@@ -23,7 +23,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 hidden_api_major_vers := 28
 ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), $(hidden_api_major_vers)),)
-    LOCAL_JAVA_LIBRARIES += telephony-common
     LOCAL_USE_AAPT2 := true
 endif
 

TransPowerBase/Android.mk
@@ -15,7 +15,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, ../common/src)
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/../common/res
 
-LOCAL_JAVA_LIBRARIES += telephony-common
 LOCAL_STATIC_ANDROID_LIBRARIES += \
     androidx.annotation_annotation \
     androidx.localbroadcastmanager_localbroadcastmanager

TransPowerProx/Android.mk
@@ -20,10 +20,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     libpower \
     libprox
 
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 28),)
-    LOCAL_JAVA_LIBRARIES += telephony-common
-endif
-
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 # proguard:

TransPowerSensors/Android.mk
@@ -22,7 +22,6 @@ LOCAL_STATIC_JAVA_LIBRARIES += libacc libpower libprox
 
 hidden_api_major_vers := 28
 ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), $(hidden_api_major_vers)),)
-    LOCAL_JAVA_LIBRARIES += telephony-common
     LOCAL_USE_AAPT2 := true
 endif
 

common/Android.mk
@@ -15,6 +15,5 @@ LOCAL_STATIC_ANDROID_LIBRARIES += \
     androidx.annotation_annotation \
     androidx.localbroadcastmanager_localbroadcastmanager
 LOCAL_STATIC_JAVA_LIBRARIES := libpower
-LOCAL_JAVA_LIBRARIES += telephony-common
 
 include $(BUILD_STATIC_JAVA_LIBRARY)