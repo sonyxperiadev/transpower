sonyxperiadev/transpower

commit 21cdf15d05f690bc5bfc78db50ff70a958136d4f

libpower: Switch to TelephonyManager.
TelephonyManager and most of its methods are part of the public system
API, not an "internal" api coming from frameworks/opt. It is also used
in other parts of transpower.

Signed-off-by: Marijn Suijten <marijns95@gmail.com>

libpower/src/com/sony/transmitpower/Transmitter.java
@@ -6,8 +6,7 @@ package com.sony.transmitpower;
 
 import android.util.Log;
 
-import com.android.internal.telephony.Phone;
-import com.android.internal.telephony.PhoneFactory;
+import android.telephony.TelephonyManager;
 
 import com.sony.transmitpower.util.OemPowerConsts;
 
@@ -26,22 +25,13 @@ public final class Transmitter {
             return;
         }
 
-        Phone phone = null;
-        try {
-            phone = PhoneFactory.getDefaultPhone();
-        } catch (IllegalStateException ex) {
-            Log.w(TAG, "getDefaultPhone exception: " + ex);
-            return;
-        }
-        if (phone == null) {
-            Log.w(TAG, "No default phone");
-            return;
-        }
+        final TelephonyManager telephonyManager = TelephonyManager.getDefault();
+        if (telephonyManager == null)
+            throw new IllegalStateException("No default telephonyManager");
 
-        byte[] request = new byte[OemPowerConsts.HEADER_SIZE
-                                  + OemPowerConsts.INT_SIZE
-                                  + OemPowerConsts.INT_SIZE];
-        ByteBuffer buf = ByteBuffer.wrap(request);
+        ByteBuffer buf = ByteBuffer.allocate(OemPowerConsts.HEADER_SIZE
+                                             + OemPowerConsts.INT_SIZE
+                                             + OemPowerConsts.INT_SIZE);
         buf.order(ByteOrder.nativeOrder());
 
         try {
@@ -59,11 +49,11 @@ public final class Transmitter {
         buf.putInt(key);
         buf.putInt(value);
 
-        try {
-            phone.invokeOemRilRequestRaw(request, null);
-        } catch (IllegalStateException e) {
-            Log.e(TAG, "OEM request exception: " + e);
-        }
+        // tm calls this through on the ITelephony service
+        byte[] resp = new byte[1024];
+        int ret = telephonyManager.invokeOemRilRequestRaw(buf.array(), resp);
+        if (ret < 0)
+            throw new IllegalArgumentException("invokeOemRilRequestRaw failed with rc = " + ret);
     }
 
     private static boolean validate(final int key, final int value) {

libpower/Android.mk
@@ -11,6 +11,4 @@ LOCAL_PROPRIETARY_MODULE := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_JAVA_LIBRARIES += telephony-common
-
 include $(BUILD_STATIC_JAVA_LIBRARY)

commit 1b93ff2862a54a0565f9f0eaf9a652e9fadf0368

libpower/transmitter: Don't consider SET_TRANSMIT_POWER failure fatal
We already know that SAR backoff doesn't work, and were previously
conveniently ignoring any failures.  The transition to TelephonyManager
in [#13] changed that and considers this to be fatal, but unless
external help comes in to guide testing on the new commands and command
structure (see open PRs) we should stick to the ignorant world and let
this misbehaviour slip through.  Which is "better" than crashing
transpower, in turn taking the telephony stack down and allowing
PartyCrasher (RescueParty, for the uninitiated) to make your phone
useless.

[#13]: https://github.com/sonyxperiadev/transpower/pull/13

Fixes: 21cdf15 ("libpower: Switch to TelephonyManager.")

libpower/src/com/sony/transmitpower/Transmitter.java
@@ -53,7 +53,7 @@ public final class Transmitter {
         byte[] resp = new byte[1024];
         int ret = telephonyManager.invokeOemRilRequestRaw(buf.array(), resp);
         if (ret < 0)
-            throw new IllegalArgumentException("invokeOemRilRequestRaw failed with rc = " + ret);
+            Log.e(TAG, "invokeOemRilRequestRaw SET_TRANSMIT_POWER failed with rc = " + ret);
     }
 
     private static boolean validate(final int key, final int value) {